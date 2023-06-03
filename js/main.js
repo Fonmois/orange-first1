1b793a488b3f27ef44d9bb649809370bc95b0c72
Fonmois <130469776+Fonmois@users.noreply.github.com>
2023-06-03 21:07:01 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+screen.orientation.lock('portrait');
+
+const nav = document.querySelector('#nav');
+const navBtn = document.querySelector('#navBtn');
+const title = document.querySelector('#title');
+
+
+navBtn.onclick = () => {
+    if(nav.classList.toggle('open')) {
+          (nav.classList.remove('d-none'), 
+          title.classList.remove('d-enable'));
+    }
+    else {
+        (nav.classList.toggle('d-none'),
+        title.classList.toggle('d-enable'));
+    }
+}
